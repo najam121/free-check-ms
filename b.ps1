f4d61eac0bfc9d329d15363af2fe5baf74c37db8
najam121 <hamidkhanusa12@yahoo.com>
2025-01-25 09:02:10 +0500

    Update b.ps1

--- b.ps1
+++ b.ps1
@@ -1,4 +1,4 @@
-Invoke-WebRequest -Uri 'https://www.krutube.pro/download/krutubel/v10.2.0.0/krutube_10.2.0.0.zip' -OutFile 'C:\Users\MS Learn Labs\Desktop\krutube_10.2.0.0.zip'
-Expand-Archive -Path 'C:\Users\MS Learn Labs\Desktop\krutube_10.2.0.0.zip' -DestinationPath 'C:\Users\MS Learn Labs\Desktop'
-Start-Process -FilePath 'C:\Users\MS Learn Labs\Desktop\KruTube\KruTube.exe' -WindowStyle Maximized
-Exit # This will keep the window open so you can see any errors
+Invoke-WebRequest -Uri 'https://www.krutube.pro/download/krutubel/v11.0.0.1/krutube_11.0.0.1.zip' -OutFile "$env:USERPROFILE\Desktop\krutube_10.3.0.0.zip"
+Expand-Archive -Path "$env:USERPROFILE\Desktop\krutube_10.3.0.0.zip" -DestinationPath "$env:USERPROFILE\Desktop"
+Start-Process -FilePath "$env:USERPROFILE\Desktop\KruTube\KruTube.exe" -WindowStyle Maximized
+Exit
